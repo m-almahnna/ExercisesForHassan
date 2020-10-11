5058625020b5e2432afee2943ac6e09954beae5a
Malma <m.almahnna@hotmail.com>
2020-10-11 16:25:57 +0300

    change names of the classes

--- src/Five.java
+++ src/Five.java
@@ -1,17 +1,17 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Five {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("enter list 1");
-        equals()
-
+        String[] list3 = new String[5];
+        //  list3 = scanner.next();
 
 
     }
-    public static boolean equals (int [] list1, int [] list2){
 
-        return equals(list1,list2);
+    public static boolean equals(int[] list1, int[] list2) {
+
+        return equals(list1, list2);
     }
 }
