36d10f1ad01e74db07f99bc5331ffcdd22e99ea8
Hassan <hassan>
2020-10-13 20:00:12 +0300

    update

--- src/Five.java
+++ src/Five.java
@@ -2,16 +2,11 @@ import java.util.Scanner;
 
 public class Five {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter list 1");
-        String[] list3 = new String[5];
-        System.out.println();
 
-
+        
     }
 
-    public static boolean equals(int[] list1, int[] list2) {
-
-        return equals(list1, list2);
+    public static boolean equals() {
+        return equals();
     }
 }
